bfb96d0c9091ebb414e31f8861c15048270f59ba
ClipsForCubesuger <57362756+ClipsForCubesuger@users.noreply.github.com>
2021-09-24 01:33:37 +0800

    Add files via upload

--- file_time.cpp
+++ file_time.cpp
@@ -1,6 +1,4 @@
 #include "file_time.h"
-#include <time.h>
-#include<stdlib.h>
 
 file_time::file_time()
 {
@@ -23,7 +21,7 @@ void file_time::gotfiletime(char * basePath)
     char base[1000];
     if ((dir=opendir(basePath)) == NULL)
     {
-       perror("Open dir error...");
+       //perror("Open dir error...");
        return;
     }
     while ((ptr=readdir(dir)) != NULL)
@@ -66,7 +64,7 @@ void file_time::changefiletime(char *basePath, file_stats *LinkHeadFrom)
     char* changeorder = new char[100];
     if ((dir=opendir(basePath)) == NULL)
     {
-       perror("Open dir error...");
+        //perror("  ");
        return;
     }
     while ((ptr=readdir(dir)) != NULL)
@@ -77,7 +75,6 @@ void file_time::changefiletime(char *basePath, file_stats *LinkHeadFrom)
        memset(changeorder,0,100);
        strcpy(changeorder,"touch -a -t ");
        char* f_time = new char[20];
-//       ltoa(next->f_stat->st_atime,f_time,20);
        sprintf(f_time,"%l",next->f_stat->st_atime);
        strcat(changeorder,f_time);
        strcat(changeorder," ");
