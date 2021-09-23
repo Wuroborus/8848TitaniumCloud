d72ec5748449aef139d875d548c1933c465a1fc1
cyk <2959363314@qq.com>
2021-09-24 03:05:44 +0800

    ok

--- file_time.cpp
+++ file_time.cpp
@@ -85,7 +85,7 @@ void file_time::changefiletime(char *basePath, file_stats *LinkHeadFrom)
        strcat(changeorder,"/");
        strcat(changeorder,ptr->d_name);
        system(changeorder);
-       next = next->next;
+       //next = next->next;
        if(ptr->d_type == 4){
            memset(base,'\0',sizeof(base));
            strcpy(base,basePath);
