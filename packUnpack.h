7fca67770f7f6581ee72fa9dc70e411019ffe12a
Springsun1s <57864317+Springsun1s@users.noreply.github.com>
2021-09-24 23:52:55 +0800

    Add files via upload

---
+++ packUnpack.h
@@ -0,0 +1,15 @@
+//
+// Created by Springsun on 2021/9/24.
+//
+#ifndef PACKUNPACK_H
+#define PACKUNPACK_H
+
+//#include <string>
+using namespace std;
+class packUnpack{
+public:
+    bool pack(const char *path,string s2);
+    bool unpack(const char *path);
+};
+
+#endif
