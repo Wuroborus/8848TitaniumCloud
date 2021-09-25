45f24f8e5924e483e1c9c99aa29de12e00402a69
Springsun1s <57864317+Springsun1s@users.noreply.github.com>
2021-09-25 20:37:51 +0800

    Add files via upload

--- lock_safe.cpp
+++ lock_safe.cpp
@@ -4,103 +4,63 @@
 #include <iostream>
 
 using namespace std;
-//char * creat_txt(){
-//    char *str=;
-//
-//    return str;
-//}
-char *s="csjknahdhkadlbk";
-int main()
-    {
 
-        FILE *fp_src1,*fp_src2;//两个文件指针,  FILE大写表示系统已经定义FILE这个变量
-        //char buffer[],buffer1[120];
-        int i, j, l, k = 0;
-        long int size;
-        FILE *fp  = fopen("key.txt", "w"); //这一行代表创建txt文件
+//const char* s = "csjknahdhkadlbk";
+bool lock(char* s,char *buffer)
+{
 
-        fprintf(fp,s);
-        //打开文件“原文件.txt”，需要加（解）密的文件，r表示读，b表示以二进制方式读，
-        if ((fp_src1 = fopen("test.txt", "rb+")) == NULL) {
-            perror("open1");
-            exit(0);
-        }
-        //打开文件“key.txt”，密钥文件，r表示读，b表示已二进制方式读，
-        if ((fp_src2 = fopen("key.txt", "rb+")) == NULL) {
-            perror("open2");
-            exit(0);
-        }
+    FILE* fp_src1, * fp_src2;
 
-        fseek(fp_src1, 0, SEEK_SET);
-        fseek(fp_src1, 0, SEEK_END);// 为了知道文件的大小
-        size = ftell(fp_src1);//ftell（）的返回值为文件的大小
-        fseek(fp_src1, 0, SEEK_SET);
-        char buffer1[size], buffer2[size];
-        do {
-            i = fread(buffer1, 1, size, fp_src1);
-            i = fread(buffer2, 1, size, fp_src2); //一个一个读文件，然后放入缓冲区
-            if (feof(fp_src1)) break;
-            // 1.feof（）这个函数是用来判断指针是否已经到达文件尾部的
-            //2.若fp已经指向文件末尾，则feof（fp）函数值为“真”，即返回非零值；否则...
-            for (i = 0; i < size; i++)
-                buffer1[i] = buffer1[i] ^ buffer2[i];  //"^" 为异或 ，异或实现加密操作
-            k++;
-            //将缓冲区的内容写入目标区
-        } while (!feof(fp_src1));
-        //打开文件“原文件.txt”，需要加（解）密的文件，w表示读，b表示已二进制方式写
-        if ((fp_src1 = fopen("test.txt", "wb+")) == NULL) {
-            perror("open5");
-            exit(0);
-        }
-        j = fwrite(buffer1, 1, size, fp_src1);
-        //关闭文件
-        fclose(fp_src1);
-        fclose(fp_src2);
+    int i, j, l, k = 0;
+    long int size;
+
+    fp_src1=fopen(s, "rb+");
+    if (fp_src1 == NULL) {
+        perror("open1");
+        exit(0);
     }
 
-//int main()
-//{
-//    FILE *fp_src1,*fp_src2;//两个文件指针,  FILE大写表示系统已经定义FILE这个变量
-//    //char buffer[],buffer1[120];
-//    int i,j,l,k=0;
-//    long int size;
-//    //打开文件“原文件.txt”，需要加（解）密的文件，r表示读，b表示以二进制方式读，
-//    if((fp_src1=fopen("test.txt","rb+"))==NULL)
-//    {
-//        perror("open1");
-//        exit(0);
-//    }
-//    //打开文件“key.txt”，密钥文件，r表示读，b表示已二进制方式读，
-//    if((fp_src2=fopen("key.txt","rb+"))==NULL)
-//    {
-//        perror("open2");
-//        exit(0);
-//    }
-//    fseek(fp_src1,0,SEEK_SET);
-//    fseek(fp_src1,0,SEEK_END);// 为了知道文件的大小
-//    size=ftell(fp_src1);//ftell（）的返回值为文件的大小
-//    fseek(fp_src1,0,SEEK_SET);
-//    char buffer1[size],buffer2[size];
-//    do
-//    {
-//        i=fread(buffer1,1,size,fp_src1);
-//        i=fread(buffer2,1,size,fp_src2); //一个一个读文件，然后放入缓冲区
-//        if (feof(fp_src1)) break;
-//        // 1.feof（）这个函数是用来判断指针是否已经到达文件尾部的
-//        //2.若fp已经指向文件末尾，则feof（fp）函数值为“真”，即返回非零值；否则...
-//        for (i=0; i<size; i++)
-//            buffer1[i]=buffer1[i]^buffer2[i];  //"^" 为异或 ，异或实现加密操作
-//        k++;
-//        //将缓冲区的内容写入目标区
-//    } while(!feof(fp_src1));
-//    //打开文件“原文件.txt”，需要加（解）密的文件，w表示读，b表示已二进制方式写
-//    if((fp_src1=fopen("test.txt","wb+"))==NULL)
-//    {
-//        perror("open5");
-//        exit(0);
-//    }
-//    j=fwrite(buffer1,1,size,fp_src1);
-//    //关闭文件
-//    fclose(fp_src1);
-//    fclose(fp_src2);
-//}
+    fseek(fp_src1, 0, SEEK_SET);
+    fseek(fp_src1, 0, SEEK_END);
+    size = ftell(fp_src1);
+    fseek(fp_src1, 0, SEEK_SET);
+    char* buffer1 = new char[size];
+    char* buffer2 = new char[size];
+//    string s1=buffer2;
+    string s2 =buffer;
+   strcpy(buffer2,s2.c_str());
+
+
+
+//    cin >> buffer2;
+//    buffer2 = (char*)s;
+    do {
+        i = fread(buffer1, 1, size, fp_src1);
+        if (feof(fp_src1)) break;
+        for (i = 0; i < size; i++)
+            buffer1[i] = buffer1[i] ^ buffer2[i];  //"^" 为异或 ，异或实现加密操作
+        k++;
+    } while (!feof(fp_src1));
+    fclose(fp_src1);
+    fp_src1=fopen(s, "wb");
+    if (fp_src1 == NULL) {
+        perror("open5");
+        exit(0);
+    }
+    j = fwrite(buffer1, 1, size, fp_src1);
+    //关闭文件
+    fclose(fp_src1);
+    return 0;
+}
+
+int main()
+
+{
+    char s[100];
+    char buffer[100];
+    cin>>s;
+//    getchar();
+    cin>>buffer;
+    lock(s,buffer);
+}
+
