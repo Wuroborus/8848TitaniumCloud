a541097f4f5be34a609037fefb23b3242e187fdf
Springsun1s <57864317+Springsun1s@users.noreply.github.com>
2021-09-26 23:33:04 +0800

    Add files via upload

--- lock_safe.cpp
+++ lock_safe.cpp
@@ -1,9 +1,10 @@
 #include "lock_safe.h"
 
+
 int FileConvert(char szOldFile[],char szNewFile[])
 {
     FILE *pOldFile=NULL;
-    FILE *pNewFile=NULL;//指针初始化为NULL
+    FILE *pNewFile=NULL;
     char cTemp;
 
     if(szOldFile==NULL||szNewFile==NULL)
@@ -11,7 +12,7 @@ int FileConvert(char szOldFile[],char szNewFile[])
         return ERR;
     }
 
-    pOldFile=fopen(szOldFile,"rb");//二进制读写文件
+    pOldFile=fopen(szOldFile,"rb");
     if(pOldFile==NULL)
     {
         return ERR;
@@ -23,30 +24,37 @@ int FileConvert(char szOldFile[],char szNewFile[])
         fclose(pOldFile);
         return ERR;
     }
-    //一定要先读取字符然后做文件末尾判断
     cTemp=fgetc(pOldFile);
-    while(!feof(pOldFile))//遇到文件结束 返回值为0
+    while(!feof(pOldFile))
     {
-//        putchar(cTemp);
+
         fputc(cTemp,pNewFile);
-        cTemp=fgetc(pOldFile)^KEY;//异或运算加密文件
+        cTemp=fgetc(pOldFile)^KEY;
 
     }
     fclose(pOldFile);
-    fclose(pNewFile);//用完文件之后需要关闭文件
+    fclose(pNewFile);
 
     return OK;
 }
 
 long GetFileSize(FILE *pf)
 {
-    //指针移到文件尾
-    fseek(pf,0,/*SEEK_END*/ 2);
+    fseek(pf,0,2);
     return ftell(pf);
 }
 
-//ping jie
+
 void pJ(char str[],char inFile[],char outFile[]){
+    for(int i=0;i<32;i++){
+        str[i]=str[i]^KEY;
+    }
+    char pd[32]={'s'};
+    for(int i=0;i<32;i++){
+        pd[i]=pd[i]^KEY;
+    }
+
+
     FILE *pWorkFile1 = NULL;
     pWorkFile1 = fopen(inFile,"rb");
     long  len=GetFileSize(pWorkFile1);
@@ -56,6 +64,8 @@ void pJ(char str[],char inFile[],char outFile[]){
     pWorkFile = fopen(inFile,"rb");
     pOutFile = fopen(outFile,"wb");
     fwrite(str,32,1,pOutFile);
+    fwrite(pd,32,1,pOutFile);
+
     unsigned char *pTmpData = NULL;
     pTmpData = new unsigned char[len];
     fread(pTmpData,len,1,pWorkFile);
@@ -66,21 +76,28 @@ void pJ(char str[],char inFile[],char outFile[]){
 }
 
 
-//chai fen
 int cF(char str[],char inFile[],char outFile[]){
+
+
     FILE *pCAB1 = NULL;
     pCAB1 = fopen(inFile,"rb");
     long all=GetFileSize(pCAB1);
     fclose(pCAB1);
     char s[32];//mi ma
+    char s1[32];
     FILE *pCAB = NULL;
     FILE *pWork = NULL;
 
     pCAB = fopen(inFile,"rb");
     fread(s,32,1,pCAB);
+    fread(s1,32,1,pCAB);
+
+    for(int i=0;i<32;i++){
+        s[i]=s[i]^KEY;
+    }
     unsigned char *pTmpData = NULL;
-    pTmpData = new unsigned char[all-32];
-    fread(pTmpData,all-32,1,pCAB);
+    pTmpData = new unsigned char[all-64];
+    fread(pTmpData,all-64,1,pCAB);
     for(int i=0;i<32;i++){
         if(str[i]!=s[i]){
             printf("password error");
@@ -88,13 +105,13 @@ int cF(char str[],char inFile[],char outFile[]){
         }
     }
     pWork = fopen(outFile,"wb");
-    fwrite(pTmpData,all-32,1,pWork);
+    fwrite(pTmpData,all-64,1,pWork);
     fclose(pWork);
     fclose(pCAB);
     return 0;
 }
 
- int code(char szOldFile[],char szNewFile[],char code[]){
+bool code(char szOldFile[],char szNewFile[],char code[]){
 
     char* s1=szOldFile;
     char s2[strlen(s1)+1];
@@ -106,24 +123,55 @@ int cF(char str[],char inFile[],char outFile[]){
     pJ(code,s2,szNewFile);
     remove(s2);
 
-    return 0;
+    return true;
 }
 
 
 
-int decode(char szOldFile[],char szNewFile[],char code[]){
+bool decode(char szOldFile[],char szNewFile[],char code[]){
     printf("++++++++++++++++++++++++++");//bu neng shan
     char s2[strlen(szOldFile)+1];
     for(int i=0;i<strlen(szOldFile);i++){
         s2[i]=szOldFile[i];
     }
     s2[strlen(szOldFile)]='1';
-    if( cF(code,szOldFile,s2)<0) return -1;;
+    if( cF(code,szOldFile,s2)<0) return false;
     FileConvert( s2, szNewFile);
     remove(s2);
 
-    return 0;
+    return true;
 }
+bool isCoded(char src[]){
+
+    FILE *pCAB = NULL;
+    pCAB = fopen(src,"rb");
+    char pd[32]={'s'};
+    for(int i=0;i<32;i++){
+        pd[i]=pd[i]^KEY;
+    }
+    char pd1[32];
+    char pd2[32];
+
+
+    fread(pd1,32,1,pCAB);
+    fread(pd2,32,1,pCAB);
+
+    for(int i=0;i<32;i++){
+        if(pd2[i]!=pd[i]){
+            printf("fei jia mi------------");
+            return false;
+        }
+
+    }
+    printf("jia mi-------------");
+
+    return true;
+
+
+}
+
+
+
 
 
 
--- lock_safe.h
+++ lock_safe.h
@@ -12,15 +12,14 @@ using namespace std;
 
 
 
+
 int FileConvert(char szOldFile[],char szNewFile[]);
 long GetFileSize(FILE *pf);
 void pJ(char str[],char inFile[],char outFile[]);
-
-//chai fen
 int cF(char str[],char inFile[],char outFile[]);
+bool code(char szOldFile[],char szNewFile[],char code[]);
+bool decode(char szOldFile[],char szNewFile[],char code[]);
 
+bool isCoded(char src[]);
 
-int code(char szOldFile[],char szNewFile[],char code[]);
-int decode(char szOldFile[],char szNewFile[],char code[]);
-
-#endif // CODE_H
+#endif 
