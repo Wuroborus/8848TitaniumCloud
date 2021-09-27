53ba1d3dad268a594abed090cbdc9b273e8deb25
kSotin <57791410+kSotin@users.noreply.github.com>
2021-09-27 08:54:43 +0800

    add a proper server

--- Server.cpp
+++ Server.cpp
@@ -23,125 +23,164 @@ int Server::make_dirs(const char *dir)
 }
 
 Server::Server() {
-    create_socket();
-    PORT = 8848;
+    port = 8848;
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
-    address_length = sizeof(address);
-
-    bind_socket();
-    set_listen();
-    accept_requests();
-}
-
-void Server::create_socket() {
-    if ((general_socket_descriptor = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("[ERROR]: Socket creation failed.");
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd < 0) {
+        std::cerr << "[ERROR] Error in socket: " << std::strerror(errno) << std::endl;
         exit(EXIT_FAILURE);
     }
-    std::cout << "[SERVER]: Socket created successfully.\n";
-}
+    std::cout << "[SERVER] Socket created successfully." << std::endl;
 
-void Server::bind_socket() {
-    if (bind(general_socket_descriptor, (struct sockaddr *)&address, sizeof(address))<0) {
-        perror("[ERROR]: Socket binding failed.");
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = port;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    e = bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    if(e < 0) {
+        std::cerr << "[ERROR] Error in bind: " << std::strerror(errno) << std::endl;
         exit(EXIT_FAILURE);
     }
-    std::cout << "[SERVER]: Socket bound successfully.\n";
-}
+    std::cout << "[SERVER] Binding successful." << std::endl;
 
-void Server::set_listen() {
-    if (listen(general_socket_descriptor, 3) < 0) {
-        perror("[ERROR]: Listening failed.");
+    if(listen(sockfd, 10) == 0){
+        std::cout << "[SERVER] Listening..." << std::endl;
+    }else{
+        std::cerr << "[ERROR] Error in listening: " << std::strerror(errno) << std::endl;
         exit(EXIT_FAILURE);
     }
-    std::cout << "[SERVER]: Socket is listening.\n";
+    addr_size = sizeof(new_addr);
 }
 
-void Server::accept_requests() {
-    if ((new_socket_descriptor = accept(general_socket_descriptor, (struct sockaddr *) &address,
-                                        (socklen_t *) &address_length)) >= 0) {
-        std::cout << "[SERVER]: Connection established.\n";
-        char buffer[1024] = {};
-        int valread = read(new_socket_descriptor, buffer, 1024);
-        buffer[valread] = '\0';
-        if (buffer[0] == 'r') {
-            send_file(buffer + 8);
-        } else if (buffer[0] == 'b') {
-            receive_file(buffer + 7);
-        } else if (buffer[0] == 'd') {
-            dup_dir(buffer + 7);
+void Server::service() {
+    new_sock = accept(sockfd, (struct sockaddr*)&new_addr, &addr_size);
+    char buffer[SIZE] = {0};
+    recv(new_sock, buffer, SIZE, 0);
+    buffer[SIZE] = '\0';
+    std::cout << "[SERVER] Requested to " << buffer << std::endl;
+    std::cout << "[SERVER] Closing the connection." << std::endl;
+    close(new_sock);
+    if (buffer[0] == 'f') {
+        receive_file(buffer + 8);
+    } else if (buffer[0] == 'd') {
+        create_dir(buffer + 7);
+    } else if (buffer[0] == 'r') {
+        restore(buffer + 8);
+    }
+}
+
+void Server::receive_file(const std::string& filepath) {
+    std::string::size_type idx = filepath.rfind('/', filepath.length());
+    std::string par_dir = filepath.substr(0, idx);
+    if (access(("./backup" + par_dir).c_str(), 00) == -1) {
+        std::cout << "[SERVER] Creating directory." << std::endl;
+        if (make_dirs(("./backup" + par_dir).c_str()) == -1) {
+            std::cerr << "[ERROR] Directory creation failed: " << std::strerror(errno) << std::endl;
+        }
+    }
+    new_sock = accept(sockfd, (struct sockaddr*)&new_addr, &addr_size);
+    char buffer[SIZE] = {0};
+    FILE *fp;
+    fp = fopen(("./backup" + filepath).c_str(), "wb");
+    int nCount;
+    while((nCount = recv(new_sock, buffer, SIZE, 0)) > 0 ){
+        fwrite(buffer, nCount, 1, fp);
+    }
+    std::cout << "[SERVER] Data written in the file successfully." << std::endl;
+    std::cout << "[SERVER] Closing the connection." << std::endl;
+    close(new_sock);
+    fflush(fp);
+    service();
+}
+
+void Server::create_dir(const std::string& dirpath) {
+    std::string::size_type idx = dirpath.rfind('/', dirpath.length());
+    std::string par_dir = dirpath.substr(0, idx);
+    if (access(("./backup" + par_dir).c_str(), 00) == -1) {
+        std::cout << "[SERVER] Creating directory." << std::endl;
+        if (make_dirs(("./backup" + par_dir).c_str()) == -1) {
+            std::cerr << "[ERROR] Directory creation failed: " << std::strerror(errno) << std::endl;
+        }
+    }
+    make_dirs(("./backup" + dirpath).c_str());
+    std::cout << "[SERVER] Directory created." << std::endl;
+    service();
+}
+
+void Server::send_file(const std::string &filepath) {
+    new_sock = accept(sockfd, (struct sockaddr*)&new_addr, &addr_size);
+    FILE *fp;
+    fp = fopen(("./backup" + filepath).c_str(), "rb");
+    if (fp == NULL) {
+        std::cerr << "[ERROR] Error in reading file: " << strerror(errno) << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    char data[SIZE] = {0};
+    int nCount;
+    while((nCount = fread(data, 1, SIZE, fp)) > 0) {
+        if (send(new_sock, data, nCount, 0) == -1) {
+            std::cerr << "[ERROR] Error in sending file: " << strerror(errno) << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        bzero(data, SIZE);
+    }
+    std::cout << "[SERVER] File sent successfully." << std::endl;
+
+    std::cout << "[SERVER] Closing the connection." << std::endl;
+    close(new_sock);
+    service();
+}
+
+void Server::request_service(const std::string& service) {
+    new_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(new_sock < 0) {
+        std::cerr << "[ERROR] Error in socket: " << strerror(errno) << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    std::cout << "[CLIENT] Socket created successfully." << std::endl;
+    e = connect(new_sock, (struct sockaddr*)&new_addr, sizeof(new_addr));
+    if(e == -1) {
+        std::cerr << "[ERROR] Error in socket: " << strerror(errno) << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    std::cout << "[CLIENT] Connected to Server." << std::endl;
+    send(new_sock, service.c_str(), service.length(), 0);
+    std::cout << "[CLIENT] Requested server to " << service << std::endl;
+    std::cout << "[CLIENT] Closing the connection." << std::endl;
+    close(new_sock);
+}
+
+void Server::restore(const std::string& source) {
+    new_addr.sin_port = clientport;
+    struct stat s;
+    if (stat(("./backup" + source).c_str(), &s) == 0) {
+        if (s.st_mode & S_IFDIR) {
+            request_service("dircpy " + source);
+            DIR *d = NULL;
+            struct dirent *dp = NULL;
+            struct stat st;
+
+            if(!(d = opendir(source.c_str()))) {
+                std::cerr << "[ERROR] Error in opening directory: " << std::strerror(errno) << std::endl;
+            }
+            while((dp = readdir(d)) != NULL) {
+                if((!strncmp(dp->d_name, ".", 1)) || (!strncmp(dp->d_name, "..", 2)))
+                    continue;
+                stat(source.c_str(), &st);
+                if(!S_ISDIR(st.st_mode)) {
+                    request_service("filecpy " + source + "/" + std::string(dp->d_name));
+                    send_file(source + "/" + std::string(dp->d_name));
+                } else {
+                    restore(source + "/" + std::string(dp->d_name));
+                }
+            }
+            closedir(d);
+        } else if (s.st_mode & S_IFREG) {
+            request_service("filecpy " + source);
+            send_file(source);
         }
     } else {
-        perror("[ERROR] : Connecting failed.");
-        exit(EXIT_FAILURE);
+        std::cerr << "[ERROR] Cannot access " << source << ": " << std::strerror(errno) << std::endl;
     }
-}
-
-void Server::send_file(std::string path) {
-    std::fstream file;
-    file.open("./backup" + path, std::ios::in | std::ios::binary);
-    if (file.is_open()) {
-        std::cout << "[SERVER]: File is ready to Transmit.\n";
-        std::string contents((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-        std::cout << "[SERVER]: Transmitting " << contents.length() << " Bytes.\n";
-        std::cout << "[SERVER]: Sending...\n";
-
-        int bytes_sent = send(new_socket_descriptor, contents.c_str(), contents.length(), 0 );
-        std::cout << "[SERVER]: Transmitted " << bytes_sent << " Bytes.\n";
-
-        std::cout << "[SERVER]: File Transfer Complete.\n";
-        close(new_socket_descriptor);
-        accept_requests();
-    } else {
-        perror("[ERROR]: File loading failed.");
-    }
-}
-
-void Server::receive_file(std::string path) {
-    std::string::size_type idx = path.rfind('/', path.length());
-    std::string par_dir = path.substr(0, idx);
-    if (access(("./backup" + par_dir).c_str(), 00) == -1) {
-        std::cout << "[SERVER]: Creating directory.\n";
-        if (make_dirs(("./backup" + par_dir).c_str()) == -1) {
-            std::cout << "[ERROR]: Directory creation failed.\n";
-        }
-    }
-    FILE* fp;
-    if ((fp = fopen(("./backup" + path).c_str(),"wb") ) == nullptr ) {
-        std::cout << "[ERROR]: File creation failed.\n";
-    }
-    std::cout << "[SERVER]: File created.\n";
-    char buffer[1024] = {};
-    int valread;
-    while(true){
-        valread = read(new_socket_descriptor, buffer, 1024);
-//        std::cout << "[SERVER]: Received " << valread <<" bytes.\n";
-        if(valread == 0)
-            break;
-//        std::cout << "[SERVER]: Saving data to file.\n";
-        fwrite(buffer, 1, valread, fp);
-    }
-    buffer[valread] = '\0';
-    fclose(fp);
-    std::cout << "[SERVER]: File saved.\n";
-    close(new_socket_descriptor);
-    accept_requests();
-}
-
-void Server::dup_dir(std::string path) {
-    std::string::size_type idx = path.rfind('/', path.length());
-    std::string par_dir = path.substr(0, idx);
-    if (access(("./backup" + par_dir).c_str(), 00) == -1) {
-        std::cout << "[SERVER]: Creating directory.\n";
-        if (make_dirs(("./backup" + par_dir).c_str()) == -1) {
-            std::cout << "[ERROR]: Directory creation failed.\n";
-        }
-    }
-    make_dirs(("./backup" + path).c_str());
-    std::cout << "[SERVER]: Directory created.\n";
-    close(new_socket_descriptor);
-    accept_requests();
 }
